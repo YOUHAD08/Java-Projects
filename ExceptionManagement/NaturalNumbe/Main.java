da17723abecf58b822b9a0223616b655c6448e72
Ayoub Youhad <ayoubyouhad79@gmail.com>
2024-12-26 02:44:11 +0100

    Add NaturalNumbe project structure and base files

---
+++ ExceptionManagement/NaturalNumbe/Main.java
@@ -0,0 +1,25 @@
+package com.oop.projects.ExceptionManagement.NaturalNumbe;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+
+            // Test the Constructor
+            Natural_Number natural_number = new Natural_Number(3);
+
+            // Test setVal() Method
+            natural_number.setVal(2);
+            natural_number.setVal(-1);
+
+            //  Test decrement() Method
+            System.out.println(natural_number.getVal());
+
+            // Retrieve Natural_Number
+            natural_number.decrement();
+        }
+        catch (NegativeNumberException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ ExceptionManagement/NaturalNumbe/NegativeNumberException.java
@@ -0,0 +1,16 @@
+package com.oop.projects.ExceptionManagement.NaturalNumbe;
+
+public class NegativeNumberException extends Exception {
+    private int false_number;
+
+
+    public NegativeNumberException(String message, int false_number) {
+        super(message);
+        this.false_number=false_number;
+    }
+
+    // Return the Number That Rise NegativeNumberException  Exception
+    public int getFalse_number() {
+        return false_number;
+    }
+}
